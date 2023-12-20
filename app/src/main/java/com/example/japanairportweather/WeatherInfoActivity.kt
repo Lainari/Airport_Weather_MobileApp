831c59532328b39ca23863610e31ee338ae67796
Lainari <108247620+Lainari@users.noreply.github.com>
2023-12-20 22:48:50 +0900

    feat : show airportNameTextView logic and image Targeting

--- app/src/main/java/com/example/japanairportweather/WeatherInfoActivity.kt
+++ app/src/main/java/com/example/japanairportweather/WeatherInfoActivity.kt
@@ -1,55 +1,46 @@
 package com.example.japanairportweather
 
-import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import com.example.japanairportweather.model.Airport
 
 class WeatherInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_info)
-
-        val icao = intent.getStringExtra("ICAO")
-        WeatherInfoTask().execute(icao)
-    }
-
-    inner class WeatherInfoTask : AsyncTask<String, Void, String>() {
-        override fun doInBackground(vararg params: String?): String {
-            val apiKey = getString(R.string.api_key)
-            val icao = params[0]
-            val url = URL("https://apis.data.go.kr/1360000/AftnAmmService/getMetar?serviceKey=$apiKey&pageNo=1&numOfRows=10&dataType=XML&icao=$icao")
-
-            val conn = url.openConnection() as HttpURLConnection
-            conn.requestMethod = "GET"
-            conn.setRequestProperty("Content-type", "application/xml")
-
-            val rd: BufferedReader
-            rd = if (conn.responseCode >= 200 && conn.responseCode <= 300) {
-                BufferedReader(InputStreamReader(conn.inputStream))
-            } else {
-                BufferedReader(InputStreamReader(conn.errorStream))
-            }
-
-            val sb = StringBuilder()
-            var line: String?
-            while (rd.readLine().also { line = it } != null) {
-                sb.append(line)
-            }
-            rd.close()
-            conn.disconnect()
-
-            return sb.toString()
+        val backButton = findViewById<Button>(R.id.backButton)
+        backButton.setOnClickListener{
+            finish()
         }
 
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            val weatherInfoTextView = findViewById<TextView>(R.id.weatherInfoTextView)
-            weatherInfoTextView.text = result // 기상 정보를 화면에 표시합니다.
+        val airport = intent.getSerializableExtra("airport") as Airport
+
+        val imageView: ImageView = findViewById(R.id.airportImageView)
+        val imageResId = resources.getIdentifier(airport?.imageName, "drawable", packageName)
+
+        if (imageResId == 0) {
+            Log.d("WeatherInfoActivity", "Invalid image name: ${airport?.imageName}")
+        } else {
+            imageView?.setImageResource(imageResId)
         }
+
+        val icaoCodeTextView: TextView? = findViewById(R.id.icaoCodeTextView)
+        icaoCodeTextView?.text = "ICAO 코드: ${airport.icaoCode}"
+
+        val airportNameTextView: TextView? = findViewById(R.id.airportNameTextView)
+        airportNameTextView?.text = "공항 이름: ${airport.airportName}"
+
+        val metarMsgTextView: TextView? = findViewById(R.id.metarMsgTextView)
+        metarMsgTextView?.text = """
+        METAR 메시지: ${airport.metarMsg}
+        보고 시간: ${airport.reportTime}
+        풍향/풍속 정보: ${airport.windInfo}
+        온도/노점온도 정보: ${airport.temperatureInfo}
+    """.trimIndent()
+
     }
 }
